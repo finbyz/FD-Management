e00c66fd793e5804aa8843e3130985daca8c1bd8
Finbyz <info@finbyz.tech>
2023-04-24 14:58:05 +0530

    filters add in account

--- fd_management/fd_management/doctype/fd_management/fd_management.js
+++ fd_management/fd_management/doctype/fd_management/fd_management.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Fd Management', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.set_query('interest_account', function(doc) {
+				return {
+					filters: {
+						"is_group": 0,
+					
+					}
+				};
+			});
+		frm.set_query('fd_account', function(doc) {
+			return {
+				filters: {
+					"is_group": 0,
+				
+				}
+			};
+		});
+		frm.set_query('bank_account', function(doc) {
+			return {
+				filters: {
+					"is_group": 0,
+				
+				}
+			};
+		});
+	}
 });
